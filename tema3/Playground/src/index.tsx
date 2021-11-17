6169285b10ce42e00d64a88d9e9263c5b138dba6
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2021-11-17 19:43:13 +0100

    Update
    Corrección Modulo

--- tema3/Playground/src/index.tsx
+++ tema3/Playground/src/index.tsx
@@ -1,11 +1,12 @@
-import logoImg from './content/foto1.jpg';
 import React from "react";
 import ReactDOM from "react-dom";
+import image  from './content/foto1.jpg';
+import './style.scss'
 
 ReactDOM.render(
   <div>
-    <h1 class="red-background">Mis primeros pasos con Reack</h1>
-    <img src={logoImg}/>
+    <h1 className="red-background">Mis primeros pasos con Reack</h1>
+    <img src={ image }/>
   </div>,
 
     
